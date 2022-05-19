a7ba26778a0cf6eb3fba0de225f4c435c03b5c55
yui0118 <yui.orange.hello@gmail.com>
2022-05-20 07:44:47 +0900

    Add: userとpostモデルにアソシエーションを追加する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # アソシエーション
+  has_many :posts
+  
   authenticates_with_sorcery!
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
